81a3fd49377cb74353a8086c4a42ce06183b6522
Missy Allan <missyallan@Melissas-MacBook-Air.local>
2016-09-07 16:53:42 -0400

    data types & Integers playground

---
+++ DataTypes - Strings & Integers.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let thridPlanet = "Earth"
+
+var ninthPlanet: String
+
+ninthPlanet = "Pluto"
+
+let fifthPlanet: String = "Jupiter"
+
+let seventhPlanet: String = "Uranus"
+
+let numberOfCountriesInAfrica = 54
+
+
+
+
+let sixthPlanet = "Saturn"
+let numberOfMoonsOfSaturn = 62
+
+
+print("There are \(numberOfMoonsOfSaturn) moons oribiting \(sixthPlanet).")
+
